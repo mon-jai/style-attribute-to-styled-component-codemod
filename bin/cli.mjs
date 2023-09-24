36ccc0193016bc7057778e3c972017b0e40092a1
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-09-25 00:36:29 +0800

    .

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
-import { execFileSync } from "child_process"
+import { spawn } from "child_process"
+import { readFile } from "fs/promises"
+
+const PACKAGE_NAME = JSON.parse(await readFile(resolve("../package.json"))).name
 
 function resolve(path) {
   return import.meta.resolve(path).replace(/^file:\/\/\//, "")
 }
 
-execFileSync(
-  "node",
-  [resolve("../node_modules/jscodeshift/bin/jscodeshift.js"), "-t", resolve("../codemod.ts"), ...process.argv.slice(2)],
-  { stdio: "inherit" }
-)
+function rebrand(output) {
+  return output.replaceAll("jscodeshift", PACKAGE_NAME)
+}
+
+const jscodeshift = spawn("node", [
+  resolve("../node_modules/jscodeshift/bin/jscodeshift.js"),
+  "-t",
+  resolve("../codemod.ts"),
+  ...process.argv.slice(2)
+])
+
+process.stdin.pipe(jscodeshift.stdin)
+
+jscodeshift.stdout.on("data", data => console.log(rebrand(data.toString())))
+jscodeshift.stderr.on("data", data => console.error(rebrand(data.toString())))
+jscodeshift.on("exit", process.exit)
