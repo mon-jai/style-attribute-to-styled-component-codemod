8ccf84e2936eb875573fde0740288d603aed9e89
mon-jai <91261297+mon-jai@users.noreply.github.com>
2023-09-25 00:54:13 +0800

    Update cli.mjs

--- bin/cli.mjs
+++ bin/cli.mjs
@@ -8,19 +8,18 @@ function resolve(path) {
   return import.meta.resolve(path).replace(/^file:\/\/\//, "")
 }
 
-function rebrand(output) {
-  return output.replaceAll("jscodeshift", PACKAGE_NAME)
+function rebrand(data) {
+  return data.toString().replaceAll("jscodeshift", PACKAGE_NAME).trim()
 }
 
-const jscodeshift = spawn("node", [
-  resolve("../node_modules/jscodeshift/bin/jscodeshift.js"),
-  "-t",
-  resolve("../codemod.ts"),
-  ...process.argv.slice(2)
-])
+const jscodeshift = spawn(
+  "node",
+  [resolve("../node_modules/jscodeshift/bin/jscodeshift.js"), "-t", resolve("../codemod.ts"), ...process.argv.slice(2)],
+  { env: { ...process.env, FORCE_COLOR: true } }
+)
 
 process.stdin.pipe(jscodeshift.stdin)
 
-jscodeshift.stdout.on("data", data => console.log(rebrand(data.toString())))
-jscodeshift.stderr.on("data", data => console.error(rebrand(data.toString())))
+jscodeshift.stdout.on("data", data => console.log(rebrand(data)))
+jscodeshift.stderr.on("data", data => console.error(rebrand(data)))
 jscodeshift.on("exit", process.exit)
